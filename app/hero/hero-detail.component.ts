13ae10a16ecf8bda4c9d7bb18ad1b69244edae53
misha-pelykh <jscript@meta.ua>
2017-01-20 21:52:52 +0200

    created normal srtucture

--- app/hero/hero-detail.component.ts
+++ app/hero/hero-detail.component.ts
@@ -2,17 +2,9 @@ import { Component, Input } from '@angular/core';
 import { Hero }             from './hero';
 
 @Component({
+  moduleId: module.id,
   selector: 'hero-detail',
-  template: `
-    <div *ngIf="hero">
-      <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name"/>
-      </div>
-    </div>
-  `
+  templateUrl: 'hero-detail.component.html'
 })
 
 export class HeroDetailComponent {
