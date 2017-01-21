4b317a0347bf6b6c30beaa0032f7173e29d7ca17
misha-pelykh <jscript@meta.ua>
2017-01-21 22:13:12 +0200

    add a few foutes ...

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,32 +1,12 @@
-import { Component }   from '@angular/core';
-import { OnInit }      from '@angular/core';
-import { Hero }        from './hero/hero';
-import { HeroService } from './hero/hero.service';
+import { Component, OnInit }   from '@angular/core';
 
 @Component({
   moduleId: module.id,
-  selector: 'my-app',
+  selector: 'app',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css'],
-  providers: [HeroService]
+  styleUrls: ['app.component.css']
 })
 
-export class AppComponent implements OnInit  {
+export class AppComponent  {
   title = 'Tour of Heroes';
-  heroes: Hero[];
-  selectedHero: Hero;
-
-  constructor(private heroService: HeroService) {}
-
-  getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
-
-  ngOnInit(): void {
-    this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,19 +1,41 @@
 import { NgModule }            from '@angular/core';
+import { RouterModule }        from '@angular/router';
 import { BrowserModule }       from '@angular/platform-browser';
 import { FormsModule }         from '@angular/forms'
+
 import { AppComponent }        from './app.component';
+import { DashboardComponent }  from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero/hero-detail.component';
+import { HeroesComponent }     from './hero/heroes.component';
+import { HeroService }         from './hero/hero.service';
 
 @NgModule({
-  imports:      [ 
+  imports: [ 
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([{
+      path: '',
+      redirectTo: '/dashboard',
+      pathMatch: 'full'
+    }, {
+      path: 'dashboard',
+      component: DashboardComponent
+    }, {
+      path: 'heroes',
+      component: HeroesComponent
+    }, {
+      path: 'detail/:id',
+      component: HeroDetailComponent
+    }])
   ],
   declarations: [ 
     AppComponent,
-    HeroDetailComponent
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
   ],
-  bootstrap:    [ AppComponent ]
+  providers: [ HeroService ],
+  bootstrap: [ AppComponent ]
 })
 
 export class AppModule {}
